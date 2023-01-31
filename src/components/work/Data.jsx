41f61bbed9bedb66400ce5da11b16eec5c46c08b
Danish Sharma <danishsharma13255@gmail.com>
2023-01-31 23:58:42 +0530

    Updated projects

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,27 +1,31 @@
 import Work1 from "../../assets/work1.jpg";
 import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.jpg";
-import Work4 from "../../assets/work4.jpg";
+import Work3 from "../../assets/Work3.png";
+import Work4 from "../../assets/Work4.png";
 
 export const projectsData = [
   {
     id: 1,
     image: Work1,
     title: "Number1",
+    link: "www.github.com",
   },
   {
     id: 2,
     image: Work2,
     title: "Number2",
+    link: "www.github.com",
   },
   {
     id: 3,
     image: Work3,
-    title: "Number3",
+    title: "Personal Portfolio",
+    link: "https://github.com/Danish1004/Danish1004.github.io",
   },
   {
     id: 4,
     image: Work4,
-    title: "Number4",
+    title: "Imdb Movie App",
+    link: "https://github.com/Danish1004/imdbmovie-app",
   },
 ];
